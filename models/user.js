658884917d83b64f96862ba73d0f3e433a7937c5
github_julienmcoding <manois.julien@gmail.com>
2021-05-28 09:06:35 +0200

    add package

--- models/user.js
+++ models/user.js
@@ -44,9 +44,8 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        notNull: { msg: 'User must have a name' },
-        notEmpty: { msg: 'Name must not be empty'},
-        isEmail: { msg: 'Must be a valid email address'}
+        notNull: { msg: 'User must have a password' },
+        notEmpty: { msg: 'Password must not be empty'},
       },
       required: true,
       len: [8,20]
